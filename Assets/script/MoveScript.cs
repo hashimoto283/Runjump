20c891ddc7a4f612ff6fb1390e403d2ca7711716
ganmo283 <kikuofive@yahoo.co.jp>
2020-09-27 16:53:04 +0900

    ジャンプ追加

--- Assets/script/MoveScript.cs
+++ Assets/script/MoveScript.cs
@@ -5,34 +5,25 @@ using UnityEngine;
 public class MoveScript : MonoBehaviour
 {
     private float STEP = 5.0f;
-    Rigidbody2D rigid2D;
     private Vector2 velocity;
     public float JUMP_POWER;
     private int MAX_JUMP_COUNT = 2;
     private int jumpCount = 0;
     private bool jump = false;
-    [SerializeField] ContactFilter2D filter2d;
+    Rigidbody2D rb;
 
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
+
+        this.transform.position += new Vector3(STEP * Time.deltaTime, 0, 0);
         if (jumpCount < MAX_JUMP_COUNT && Input.GetMouseButtonDown(0))
         {
-            jump = true;
-        }
-        if (jump)
-        {
-            // 速度をゼロにして、2回目のジャンプも1回目と同じ挙動にする
-            this.rigid2D = GetComponent<Rigidbody2D>();
-            rigid2D.velocity = Vector2.zero;
-
-            //ジャンプさせる
-            this.rigid2D.AddForce(transform.up * JUMP_POWER);
-
-            //ジャンプ回数をカウント
-            jumpCount++;
-
-            jump = false;
+           rb.AddForce(new Vector2(0, JUMP_POWER));
         }
     }
 }
